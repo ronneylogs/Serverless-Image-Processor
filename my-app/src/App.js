56eee8272a0b200b32968171504da243adbf95fa
Ronney Lok <ronneylok@gmail.com>
2023-07-22 02:07:07 -0700

    added download features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {useState,useRef} from 'react'
+import { saveAs } from 'file-saver';
 import {
   ChakraProvider,
   Box,
@@ -9,26 +11,76 @@ import {
   Grid, GridItem,
   Button, ButtonGroup,
   theme,
+  Image,
+  Center,
+  AspectRatio,
+  Input,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-import ImageUpload from './ImageUpload';
+// import ImageUpload from './ImageUpload';
+
 
 function App() {
+
+  const inputRef = useRef(null);
+    const [image, setImage] = useState('');
+
+  function handleImage(e){
+        console.log(e.target.file);
+        setImage(e.target.files[0]);
+  }
+
+  //function for saving file
+
+
+
   return (
     <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl" m={5}>
-      <Text fontSize='4xl'>Serverless Image Processor</Text>
-      <Grid templateColumns='repeat(2, 1fr)' gap={6}>
-        <GridItem  w='100%' h='10' >
-          <Button borderRadius='md' border='1px' border-color='black.200' bg='gray.200' size='md' w='100%'>
-            <Text fontSize='2xl'>Select a photo</Text>
-          </Button>
-        </GridItem>
-        <GridItem w='100%' h='10' bg='black' />
-      </Grid>
-    </Box>
-     <ImageUpload></ImageUpload>
+      <Box textAlign="center" fontSize="xl" m={5}>
+        <Text fontSize='4xl'>Serverless Image Processor</Text>
+        <Grid templateColumns='repeat(2, 1fr)' gap={6}>
+          <GridItem  w='100%' h='10' >
+              <Input 
+              p={1}
+              _placeholder={{ opacity: 1, color: 'black' }}
+              placeholder={"hi"}
+              borderRadius='md' 
+              border='1px' 
+              border-color='black.200' 
+              type='file'
+              name='file' 
+              ref={inputRef} 
+              onChange={handleImage}></Input>
+          </GridItem>
+          <GridItem w='100%' h='10' bg='blue.400' borderRadius='md' border='1px' >
+            <Button w='100%' h='10' bg='blue.400' borderRadius='md' border='1px' >
+              {/* <Text>Download</Text> */}
+               {
+                  image?(
+                    <Link w='100%' href={URL.createObjectURL(image)} download>Click to download</Link>
+                      
+
+                  ): <Link >No Image</Link>
+              }
+              {/* <a href={URL.createObjectURL(image)} download>Click to download</a> */}
+            </Button>
+          </GridItem>
+        </Grid>
+      </Box>
+       <Center m={5} alignItems='center' >
+            <Box w='50%'>
+              {
+                  image?(
+                      <AspectRatio ratio={9/6}>
+                          <Image src={URL.createObjectURL(image)}></Image>
+                      </AspectRatio>
+
+                  ): <img src='./logo.svg'></img>
+              }
+            </Box>
+        </Center>
+     
     </ChakraProvider>
   );
 }
