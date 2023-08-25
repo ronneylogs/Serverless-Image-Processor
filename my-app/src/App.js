0de60249bdb96d67fe121b7f2dfae7392c71040a
Ronney Lok <ronneylok@gmail.com>
2023-08-25 13:37:37 -0700

    Connected to aws backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useState,useRef} from 'react'
+import axios from "axios";
 import { saveAs } from 'file-saver';
 import {
   ChakraProvider,
@@ -31,17 +32,88 @@ function App() {
         setImage(e.target.files[0]);
   }
 
-  //function for saving file
+  function invertApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('invertUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
 
+  function flipHorizontalApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('flipHorizontalUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
+
+  function flipVerticalApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('flipVerticalUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
+
+   function increaseBApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('increaseBUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
+
+  function decreaseBApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('decreaseBUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
+
+   function sepiaApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('sepiaUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
+
+  function pixelateApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('pixelateUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
+
+  function binarizeApi(){
+    const formData = new FormData();
+    formData.append("img", image);
+    axios.get('binarizeUrl',formData).then((res) => {
+      console.log(res);
+      setImage(res);
+    });
+  }
 
 
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl" m={5}>
-        <Text fontSize='4xl'>Serverless Image Processor</Text>
+      <Box as='b' textAlign="center" fontSize="xl" >
+        <Box bg='whiteAlpha.400' boxShadow='md' mb={7} py={10} >
+          <Text fontSize='4xl'>Serverless Image Processor</Text>
+        </Box>
         <Grid templateColumns='repeat(2, 1fr)' gap={6}>
           <GridItem  w='100%' h='10' >
-              <Input 
+                            <Input 
               p={1}
               _placeholder={{ opacity: 1, color: 'black' }}
               placeholder={"hi"}
@@ -52,6 +124,7 @@ function App() {
               name='file' 
               // ref={inputRef} 
               onChange={handleImage}></Input>
+
           </GridItem>
           <GridItem w='100%' h='10' bg='blue.400' borderRadius='md' border='1px' >
             <Button w='100%' h='10' bg='blue.400' borderRadius='md' border='1px' >
@@ -83,19 +156,18 @@ function App() {
               }
             </Box>
                     <Box>
-          <Button m={5} bg='orange'>Invert</Button>
-          <Button m={5} bg='orange'>Flip Horizontal</Button>
-          <Button m={5} bg='orange'>Flip Vertical</Button>
+          <Button onClick={invertApi} m={5} bg='orange'>Invert</Button>
+          <Button onClick={flipHorizontalApi} m={5} bg='orange'>Flip Horizontal</Button>
+          <Button onClick={flipVerticalApi} m={5} bg='orange'>Flip Vertical</Button>
       
 
+          <Button onClick={decreaseBApi} m={5} bg='orange'>Decrease Brightness</Button>
+          <Button onClick={increaseBApi} m={5} bg='orange'>Increase Brightness</Button>
+          <Button onClick={sepiaApi} m={5} bg='orange'>Sepia Filter</Button>
 
-          <Button m={5} bg='orange'>Decrase Brightness</Button>
-          <Button m={5} bg='orange'>Increase Brightness</Button>
-          <Button m={5} bg='orange'>Sepia Filter</Button>
 
-
-          <Button m={5} bg='orange'>Pixelate</Button>
-          <Button m={5} bg='orange'>Binarize</Button>
+          <Button onClick={pixelateApi} m={5} bg='orange'>Pixelate</Button>
+          <Button onClick={binarizeApi} m={5} bg='orange'>Binarize</Button>
         </Box>
             
         </Center>
